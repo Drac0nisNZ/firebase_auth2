e5d249eeb692259f4de2f756888bebbc58eb41c8
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2023-10-30 12:19:02 +1300

    firebase Auth setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+//TODO - add your firebase sdk that you want to use
+
+//your web app firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyDogUoZY9pZD9J1Fw-BADazJPgVLSDX4v4",
+    authDomain: "fir-auth-4d7ee.firebaseapp.com",
+    projectId: "fir-auth-4d7ee",
+    storageBucket: "fir-auth-4d7ee.appspot.com",
+    messagingSenderId: "527780963754",
+    appId: "1:527780963754:web:5742072c5b43f8a95df7d3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  //const analytics = firebaseApp.getanalytics(app);
